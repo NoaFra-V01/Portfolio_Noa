c8eb08eb46805a9a64afe37b89841b57c70e7585
Watergg <noa.fraigneau.ducruet@gmail.com>
2025-12-15 00:13:19 +0100

    Ajout apparence descente

--- script.js
+++ script.js
@@ -65,3 +65,25 @@ accordions.forEach(item => {
         }
     });
 });
+
+
+const ratio = .1; 
+const options = {
+  root: null,
+  rootMargin: '0px',
+  threshold: ratio
+}
+
+const handleIntersect = function (entries, observer) {
+  entries.forEach(function (entry) {
+    if (entry.intersectionRatio > ratio) {
+      entry.target.classList.add('reveal-visible');
+      observer.unobserve(entry.target);
+    }
+  });
+}
+
+const observer = new IntersectionObserver(handleIntersect, options);
+document.querySelectorAll('.reveal').forEach(function (r) {
+  observer.observe(r);
+});
